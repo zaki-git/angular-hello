fc99d5d4ec5e3e54ab053d4e923ce099750c90a5
Zaki Choudhury <zaki.cse.10@gmail.com>
2019-07-16 10:36:39 +0600

    test commit
    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'greetings from angular-hello';
+  title = 'greetings from angular-hello updated';
 }
