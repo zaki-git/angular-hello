2e4cdc9db38c3a6ab048ee4aac643e46c93cde06
Zaki Choudhury <zaki.cse.10@gmail.com>
2019-07-16 16:48:42 +0600

    added a new greetings component

---
+++ src/app/components/greeting.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'greeting',
+    templateUrl: './greeting.component.html',
+    styleUrls: ['./greeting.component.css']
+    
+})
+export class GreetingComponent {
+    time : any;
+
+    constructor(){
+        this.time = (new Date()).toLocaleTimeString();
+    }
+
+    public updateTime() : void {
+        this.time = (new Date()).toLocaleTimeString();
+    }
+}
+
