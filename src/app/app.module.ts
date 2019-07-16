32fb8846d61698e625479f600906ccc72c0370f1
Zaki Choudhury <zaki.cse.10@gmail.com>
2019-07-16 16:49:13 +0600

    used the new GreetingsComponent in AppComp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreetingComponent } from './components/greeting.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, GreetingComponent
   ],
   imports: [
     BrowserModule,
