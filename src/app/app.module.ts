b62bdf58c88e1c7a6643348dff95c05ce9c7d10f
Zaki Choudhury <zaki.cse.10@gmail.com>
2020-03-09 18:30:28 +0600

    added newly added component and used modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './components/greeting.component'
+import { ColorComponent } from './components/color.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent, GreetingComponent
+    AppComponent, GreetingComponent, ColorComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
