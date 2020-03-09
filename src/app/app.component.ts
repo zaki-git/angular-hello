e47f664def70bd956139a3cbd846abc1b340cf55
Zaki Choudhury <zaki.cse.10@gmail.com>
2020-03-09 18:30:04 +0600

    trying to change the color of color component from parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playground';
+  title = 'Playground';
+  public textValue = 'some text';
+
+  public SetColor() {
+    console.debug('THe text value is: ' + this.textValue);
+  }
+
 }
+
+
