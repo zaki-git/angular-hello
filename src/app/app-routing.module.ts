ff3c9ebdf6c0363dccb4d99b789f8963687fe52b
Zaki Choudhury <zaki.cse.10@gmail.com>
2020-03-28 14:24:05 +0600

    working on implementing basic routing in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/Greetings/landing.component';
+import{ ComponentInteraction } from './components/ComponentInteraction/component-interaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'component-interaction', component: ComponentInteraction }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
